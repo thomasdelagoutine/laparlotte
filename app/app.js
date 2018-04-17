2af33c73b52c1091726e5727bc6941728fae6318
thomasdelagoutine <thomasdelagoutine@gmail.com>
2018-04-17 22:23:30 +0200

    Connection avec firebase et ajout de SoundCloud

--- app/app.js
+++ app/app.js
@@ -25,3 +25,12 @@ app.config(['$routeProvider',
             });
     }
 ]);
+
+app.config(function($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+        // Allow same origin resource loads.
+        'self',
+        // Allow loading from our assets domain.  Notice the difference between * and **.
+        'https://w.soundcloud.com/player/**'
+    ]);
+});
--- app/mainController.js
+++ app/mainController.js
@@ -6,8 +6,34 @@ app.run(function ($rootScope) {
 });
 
 
-app.controller('mainCtrl', ['$scope', '$location', '$firebaseObject','$firebaseAuth',
-    function ($scope, $location, $firebaseObject, $firebaseAuth) {
+
+app.controller('mainCtrl', ['$scope', '$location', '$firebaseObject', '$firebaseAuth', '$sce',
+    function ($scope, $location, $firebaseObject, $firebaseAuth, $sce) {
+        //Initialisation
+        var ref = firebase.database().ref();
+        _getProfileInfo().then(function (aTabEvent) {
+            $scope.tabEvents = aTabEvent;
+            console.log($scope.tabEvents);
+            $scope.$apply();
+        });
+
+        /**
+         * Fonction permettant de récuperer la liste des evenements.
+         * @returns {*}
+         * @private
+         */
+        function _getProfileInfo() {
+            return firebase.database().ref('/events/').once('value').then(function (pEvents) {
+                var tabEvents = [];
+                var aEvents = pEvents.val();
+                angular.forEach(aEvents, function (aEvent) {
+                    $sce.trustAsResourceUrl(aEvent.linkSoundCloud);
+                    tabEvents.push(aEvent);
+                });
+                return tabEvents;
+            });
+        }
+
     }]);
 
 
